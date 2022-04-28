6da42dbfa2a7d7f583c9d49b6eca30b4d82e4d31
mutehayyiz <ahmet_senharputlu@hotmail.com>
2022-04-29 01:48:18 +0300

    Add router

---
+++ lib/controller/router.dart
@@ -0,0 +1,34 @@
+import 'package:business_wallet/screens/contacts.dart';
+import 'package:business_wallet/screens/profile.dart';
+import 'package:flutter/material.dart';
+import '../screens/event.dart';
+import '../screens/qr.dart';
+
+class PageRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case event:
+        return MaterialPageRoute(builder: (_) => const Event());
+      case qr:
+        return MaterialPageRoute(builder: (_) => const QR());
+      case profile:
+        return MaterialPageRoute(builder: (_) => const Profile());
+      case contacts:
+        return MaterialPageRoute(builder: (_) => const Contacts());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            appBar: AppBar(),
+            body: const Center(
+              child: Text('Something is wrong!'),
+            ),
+          ),
+        );
+    }
+  }
+}
+
+const String event = '/event';
+const String qr = '/qr';
+const String profile = '/profile';
+const String contacts = '/contacts';
